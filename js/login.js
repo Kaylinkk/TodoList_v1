3db33fd9dd2ff47407cecb6719d3fd0511a98c5e
Kaylinkk <62476447+Kaylinkk@users.noreply.github.com>
2021-08-31 22:12:18 +0800

    JS:sign out

--- js/login.js
+++ js/login.js
@@ -3,6 +3,10 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 const inside = document.querySelector("#insideBox");
 const formationBox = document.querySelector(".formBox");
+
+const signOutbutton = document.getElementById("sign-out");
+
+
 const HIDDEN_CLASS ="hidden"
 const USERNAME_KEY ="username"
 
@@ -19,7 +23,8 @@ function welcomeSign(username){
     greeting.innerText = `Hello, ${username}`
     greeting.classList.remove(HIDDEN_CLASS);
     inside.classList.remove(HIDDEN_CLASS);
-    formationBox.classList.toggle("formBox");
+    formationBox.classList.remove("formBox");
+    signOutbutton.classList.remove(HIDDEN_CLASS);
     
     }
 
@@ -30,10 +35,19 @@ function welcomeSign(username){
     if (savedUsername === null){
         loginForm.classList.remove(HIDDEN_CLASS);
         inside.classList.add(HIDDEN_CLASS);
-    loginForm.addEventListener("submit" ,loginSubmit);
+        loginForm.addEventListener("submit" ,loginSubmit);
+        signOutbutton.classList.add("hidden");
+
     } else{
         loginForm.classList.add(HIDDEN_CLASS);
         greeting.classList.remove(HIDDEN_CLASS);
+        signOutbutton.classList.remove(HIDDEN_CLASS);
         welcomeSign(savedUsername);
-       
     }
+
+    function handleSignOut(){
+      localStorage.removeItem(USERNAME_KEY);
+      window.location.reload();
+}
+
+signOutbutton.addEventListener("click", handleSignOut);
